7df48693f2ac6aca23f36e187eabb983199eb9d9
John Astill <john@astill.org>
2023-02-03 22:26:16 -0500

    starting day3

--- cpp/src/main.cpp
+++ cpp/src/main.cpp
@@ -18,120 +18,9 @@ bool compareElves(Elf *e1, Elf *e2)
     return e1->getTotalCalories() > e2->getTotalCalories();
 }
 
-void day1()
+void day3()
 {
-    Elf *elf;
-    elf = new Elf();
-    elves.push_back(elf);
-
-    char line[64];
-    while (inputFile.getline(line, 64))
-    {
-        cout << line << endl;
-        // If the line is empty, it's a new elf.
-        if (line[0] == '\0')
-        {
-            elf = new Elf();
-            elves.push_back(elf);
-        }
-        else
-        {
-            elf->addCalories(atoi(line));
-        }
-    }
-
-    int totalCalories = 0;
-
-    // Sort the elves by calories
-    std::sort(elves.begin(), elves.end(), compareElves);
-    for (int i = 0; i < TOP_ELVES; i++)
-    {
-        std::cout << "Elf " << i << " has " << elves[i]->getTotalCalories() << " calories" << std::endl;
-        totalCalories += elves[i]->getTotalCalories();
-    }
-
-    std::cout << "Total calories: " << totalCalories << std::endl;
-
 }
-
-/**
- * @brief This is an ugly implementaiton that decodes the two sets of values.
- * but it is functional and simple to understand....
- * 
- * @param elf 
- * @param me 
- * @return int 
- */
-int rps(char elf, char me)
-{
-    std::cout << "Elf: " << elf << " Me: " << me << std::endl;
-    if (elf == 'A')
-    {
-        if (me == 'X')
-        {
-            return 3 + 1;
-        }
-        else if (me == 'Y')
-        {
-            return 6 + 2;
-        }
-        else if (me == 'Z')
-        {
-            return 0 + 3;
-        }
-    }
-    else if (elf == 'B')
-    {
-        if (me == 'X')
-        {
-            return 0 + 1;
-        }
-        else if (me == 'Y')
-        {
-            return 3 + 2;
-        }
-        else if (me == 'Z')
-        {
-            return 6 + 3;
-        }
-    }
-    else if (elf == 'C')
-    {
-        if (me == 'X')
-        {
-            return 6 + 1;
-        }
-        else if (me == 'Y')
-        {
-            return 0 + 2;
-        }
-        else if (me == 'Z')
-        {
-            return 3 + 3;
-        }
-    }
-    return 0;
-
-}
-void day2()
-{
-    int totalPoints = 0;
-    char line[4];
-
-    while (inputFile.getline(line, 4))
-    {
-        // If the line is empty, it's a new elf.
-        if (line[0] != '\0')
-        {
-            char elf = line[0];
-            char me = line[2];
-
-            totalPoints += rps(elf, me);
-        }
-    }
-    std::cout << "Total points: " << totalPoints << endl;
-}
-
 int main(int argc, char *argv[])
 {
 
@@ -152,7 +41,7 @@ int main(int argc, char *argv[])
         return 1;
     }
 
-    day2();
+    day3();
 
     // Dispose of elves
     for (int i = 0; i < elves.size(); i++)
